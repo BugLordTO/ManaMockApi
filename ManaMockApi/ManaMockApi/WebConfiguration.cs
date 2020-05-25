746b53391f2ff9b2bbea1c67b8ae3dfeb86172f0
Somkiet <somsor@perfenterprise.com>
2020-05-25 15:31:00 +0700

    add WebConfiguration

---
+++ ManaMockApi/ManaMockApi/WebConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManaMockApi
+{
+    public class WebConfiguration
+    {
+        public string StorageAccountConnectionString { get; set; }
+        public string StorageAccountBaseUrl { get; set; }
+        public string ProductContainerName { get; set; }
+    }
+}
